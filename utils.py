e31a7b1b293502be7e54b8d65bdb2328166b0da8
Ashafix <maximili.peters@mail.huji.ac.il>
2020-01-04 23:54:33 +0100

    more utility functions

--- utils.py
+++ utils.py
@@ -1,5 +1,81 @@
 import os
 from PIL import Image
+import os
+import shutil
+import imagehash
+from PIL import Image
+import numpy as np
+import yaml
+import time
+
+
+def get_false_positives():
+
+    direc = os.path.join(os.getcwd(), 'classifiers', 'round_passed_real_cases')
+    files = os.listdir(direc)
+    hashes = list()
+    for file in files:
+        if file.endswith('.png') and not os.path.isfile(os.path.join(os.path.split(file)[0], "true_positives", os.path.split(file)[1])):
+            img = Image.open(os.path.join(direc, file))
+            hashes.append(str(imagehash.phash(img.crop((0, 0, 250, 100)), hash_size=5)))
+        elif file.endswith('.png') and os.path.isfile(os.path.join(os.path.split(file)[0], "true_positives", os.path.split(file)[1])):
+            os.remove(file)
+    hashes = list(set(hashes))
+    print(', '.join(hashes))
+    with open('hashes.txt', 'w') as f:
+        f.write(', '.join(hashes))
+
+    return hashes
+
+
+def get_true_positives():
+
+    direc = os.path.join(os.getcwd(), 'classifiers', 'round_passed_real_cases', 'true_positives')
+    files = os.listdir(direc)
+    hashes = list()
+    for file in files:
+        if file.endswith('.png'):
+            img = Image.open(os.path.join(direc, file))
+            hashes.append(str(imagehash.phash(img.crop((0, 0, 250, 100)), hash_size=5)))
+    hashes = list(set(hashes))
+    print(', '.join(hashes))
+    return hashes
+
+
+def write_false_positives():
+
+    hashes = get_false_positives()
+    with open('NeuroMarioConfig.yaml', 'r') as f:
+        conf = yaml.load(f)
+    conf['finish_line_false_positives'] = hashes
+    hashes = get_true_positives()
+    conf['finish_line_true_positives'] = hashes
+    with open('NeuroMarioConfig.yaml', 'w') as f:
+        yaml.dump(conf, f)
+    for true_pos in conf['finish_line_true_positives']:
+        if true_pos in conf['finish_line_false_positives']:
+            raise ValueError('hash collision: {}'.format(true_pos))
+
+
+def delete_all_good_runs(direc="", suffix=".weights-2"):
+
+    if not os.path.isdir(direc):
+        direc = os.path.join(os.getcwd(), direc)
+    files = os.listdir(direc)
+    files = [os.path.join(direc, file) for file in files]
+    for file in files:
+        print('checking file: {}'.format(file))
+        if file.endswith('.txt'):
+            with open(file, 'r') as f:
+                try:
+                    t = float(f.read().strip())
+                except:
+                    t = 100
+                print(t)
+            if t < 100 and file.replace('.txt', suffix) in files and not file.replace('.txt', '.finished') in files:
+                print('Deleting file: {}'.format(file))
+                os.remove(file)
+                os.remove(file.replace('.txt', suffix))
 
 
 def trim_image(images, player=1, prefix='trimmed_', overwrite=True, remove_timer=True, save_file=False):
@@ -76,3 +152,13 @@ def remove_redundant_files(filenames, pressed_buttons):
             filenames.pop(i)
             pressed_buttons.pop(i)
     return filenames, pressed_buttons
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        t0 = time.time()
+        result = method(*args, **kw)
+        t1 = time.time()
+        print('{}  {} ms'.format(method.__name__, (t1 - t0) * 1000))
+        return result
+    return timed
