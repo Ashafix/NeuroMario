93fedcc79c2cc8385eae9067344e5e01aedda86c
Ashafix <maximili.peters@mail.huji.ac.il>
2020-01-06 11:07:48 +0100

    more flexible printer, dummy printer

--- Printer.py
+++ Printer.py
@@ -1,7 +1,26 @@
+import sys
+
+
 class Printer:
-    def __init__(self, verbose=False):
+    def __init__(self, verbose=False, target=sys.stdout):
         self.verbose = verbose
+        self.target = target
 
     def log(self, msg, verbose=None):
-        if (verbose is True) or (self.verbose and verbose is not False):
-            print(msg)
+        if verbose or (self.verbose and not verbose):
+            if self.target is not None:
+                msg = str(msg)
+                self.target.write(msg)
+                self.target.write('\n')
+                self.target.flush()
+            else:
+                self.print(msg)
+
+
+class PrinterDummy(Printer):
+    def __init__(self, verbose=False, target=None):
+        self.verbose = verbose
+        self.target = target
+
+    def log(self, msg, verbose=None):
+        return None
