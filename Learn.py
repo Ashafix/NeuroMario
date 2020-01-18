bb3df84c950fc265fa3f001055a36e55c40f82eb
Ashafix <maximili.peters@mail.huji.ac.il>
2020-01-18 20:24:36 +0100

    folder became attribute, predict_finshline and on_img_error can be functions

--- Learn.py
+++ Learn.py
@@ -5,18 +5,29 @@ import hashlib
 import pickle
 from GameServer import GameServer
 from Emuhawk import Emuhawk
+from Joypad import get_missing_values
 import utils
 
 
 class Learn:
-    def __init__(self, model, weights_index, args, best_score=21.19):
+    def __init__(self, model, weights_index, args, best_score=21.19, predict_finishline=None):
         self.best_score = best_score
         self.model = model
         self.initial_weights = model.get_weights()
         self.weights_index = weights_index
         self.initial_shape = self.initial_weights[weights_index].shape
-        self.state = None
+        self.state = args.state
+        if predict_finishline:
+            self.predict_finishline = predict_finishline
+        else:
+            self.predict_finishline = args.predict_finishline
         self.args = args
+        if self.state is None:
+            self.folder = 'weights'
+        else:
+            self.folder = 'weights_{}'.format(self.state)
+        if not os.path.isdir(os.path.join(os.getcwd(), self.folder)):
+            pathlib.Path(os.path.join(os.getcwd(), self.folder)).mkdir(parents=True, exist_ok=True)
 
     def learn(self, weights, keep_hash=True):
         """
@@ -25,13 +36,6 @@ class Learn:
         :param keep_hash:
         :return:
         """
-        print(weights)
-        if self.state is None:
-            folder = 'weights'
-        else:
-            folder = 'weights_{}'.format(self.state)
-        if not os.path.isdir(os.path.join(os.getcwd(), folder)):
-            pathlib.Path(os.path.join(os.getcwd(), folder)).mkdir(parents=True, exist_ok=True)
 
         _weights = self.model.get_weights()
         if keep_hash:
@@ -39,16 +43,18 @@ class Learn:
         else:
             adjusted_weights = weights
         _weights[self.weights_index] = adjusted_weights.reshape(self.initial_shape)
-        self.model.set_weights(_weights)
+
         if keep_hash:
             hash_digest = hashlib.sha1(_weights[self.weights_index]).hexdigest()
-            filename = '{}/{}.txt'.format(folder, hash_digest)
+            filename = '{}/{}.txt'.format(self.folder, hash_digest)
             if os.path.isfile(filename):
                 with open(filename, 'r') as f:
                     score = float(f.read().strip())
                     print(filename)
                     print('retrieving hashed score: {}'.format(score))
                     return score
+
+        self.model.set_weights(_weights)
         g = GameServer(socket_autostart=True,
                        socket_ip=socket.gethostbyname(socket.gethostname()),
                        socket_port=9000 + os.getpid() % 1000)
@@ -59,20 +65,26 @@ class Learn:
         e.state = self.state
         e.start()
         g.server.create_connection(timeout=60)
-        missing = utils.get_missing_values(self.model)
+        missing = get_missing_values(self.model)
+        if self.predict_finishline:
+            predictor_finish_line = None
+            on_img_error = None
+        else:
+            predictor_finish_line = lambda _: False
+            on_img_error = utils.msg_to_time
 
         x = g.ai(method='socket',
                  model=self.model,
                  missing=missing,
                  threshold=self.args.play_threshold,
                  bit_array=self.args.bit_array,
-                 predictor_finish_line=lambda x: False,
-                 on_img_error=utils.msg_to_time)
+                 predictor_finish_line=predictor_finish_line,
+                 on_img_error=on_img_error)
 
         if keep_hash:
-            with open('{}/{}.txt'.format(folder, hash_digest), 'w') as f:
+            with open('{}/{}.txt'.format(self.folder, hash_digest), 'w') as f:
                 f.write(str(x))
-            with open('{}/{}.weights-{}'.format(folder, hash_digest, self.weights_index), 'wb') as f:
+            with open('{}/{}.weights-{}'.format(self.folder, hash_digest, self.weights_index), 'wb') as f:
                 pickle.dump(_weights[self.weights_index], f)
 
         print(x)
